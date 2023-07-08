0d57938d6b812be4149384359e17e464740b45ff
rahman <rahmandevbd@gmail.com>
2023-07-08 21:16:59 +0600

    add graphbase config

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { g, auth, config } from '@grafbase/sdk';
+
+const User = g.model('user', {
+  name: g.string().length({ min: 2, max: 20 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 4, max: 50 }),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(),
+  project: g
+    .relation(() => Project)
+    .list()
+    .optional()
+});
+
+const Project = g.model('Project', {
+  title: g.string().length({ min: 2, max: 20 }),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User)
+});
+
+export default config({
+  schema: g
+  // Integrate Auth
+  // https://grafbase.com/docs/auth
+  // auth: {
+  //   providers: [authProvider],
+  //   rules: (rules) => {
+  //     rules.private()
+  //   }
+  // }
+});
