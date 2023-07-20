736c0d2d05f6563001134497486393b0db01bccf
rahman <rahmandevbd@gmail.com>
2023-07-20 16:04:48 +0600

    fix image issue

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -28,15 +28,11 @@ const ProjectCard = ({ id, image, title, name, avatarUrl, userId }: Props) => {
     <div className="flexCenter flex-col rounded-2xl drop-shadow-card">
       <Link
         href={`/project/${id}`}
-        className="flexCenter group relative w-full h-full"
+        className="flexCenter group relative w-full h-full "
       >
-        <Image
-          src={image}
-          width={414}
-          height={314}
-          className="w-full h-full object-cover rounded-2xl"
-          alt="project image"
-        />
+        <div className="">
+          <Image src={image} width={414} height={314} alt="project image" />
+        </div>
 
         <div className="hidden group-hover:flex profile_card-title">
           <p className="w-full">{title}</p>
